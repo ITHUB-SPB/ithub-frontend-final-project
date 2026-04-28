927be95a045f2530f3cea43039f904ef7ead3d6c
Lex Trofimov <daslef93@gmail.com>
2026-04-28 13:59:22 +0300

    wip

---
+++ packages/crawler/src/config.ts
@@ -0,0 +1,41 @@
+import type { PlaywrightCrawlerOptions } from 'crawlee';
+import { BrowserName, DeviceCategory, OperatingSystemsName } from '@crawlee/browser-pool';
+import { launchOptions } from 'camoufox-js';
+import { firefox } from 'playwright-extra';
+import stealthPlugin from 'puppeteer-extra-plugin-stealth';
+
+firefox.use(stealthPlugin());
+
+export const crawlerDefault: PlaywrightCrawlerOptions = {
+    postNavigationHooks: [
+        async ({ handleCloudflareChallenge }) => {
+            await handleCloudflareChallenge();
+        },
+    ],
+    browserPoolOptions: {
+        useFingerprints: true,
+        fingerprintOptions: {
+            fingerprintGeneratorOptions: {
+                browsers: [
+                    {
+                        name: BrowserName.firefox,
+                        minVersion: 96,
+                    },
+                ],
+                devices: [DeviceCategory.desktop],
+                operatingSystems: [OperatingSystemsName.windows],
+            },
+        },
+    },
+    launchContext: {
+        launcher: firefox,
+        launchOptions: await launchOptions({
+            headless: true,
+        }),
+    },
+    failedRequestHandler({ request, log }) {
+        log.error(`Request ${request.url} failed too many times.`);
+    },
+    maxRequestsPerCrawl: 10,
+    headless: false,
+};
--- packages/crawler/src/index.ts
+++ packages/crawler/src/index.ts
@@ -1,85 +1,110 @@
-import { PlaywrightCrawler, FileDownload } from 'crawlee';
-import { BrowserName, DeviceCategory, OperatingSystemsName } from '@crawlee/browser-pool';
-import { launchOptions } from 'camoufox-js';
-import { firefox } from 'playwright-extra';
-import stealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { PlaywrightCrawler, FileDownload, type PlaywrightCrawlingContext } from 'crawlee';
+import { crawlerDefault } from './config.js';
 
-firefox.use(stealthPlugin());
 
 const downloadCrawler = new FileDownload({
     async requestHandler({ body, request, contentType, getKeyValueStore }) {
         const url = new URL(request.url);
         const kvs = await getKeyValueStore();
-
         await kvs.setValue(url.pathname.replace(/\//g, '_'), body, { contentType: contentType.type });
     },
 });
 
-const crawler = new PlaywrightCrawler({
-    postNavigationHooks: [
-        async ({ handleCloudflareChallenge }) => {
-            await handleCloudflareChallenge();
-        },
-    ],
-    browserPoolOptions: {
-        useFingerprints: true,
-        fingerprintOptions: {
-            fingerprintGeneratorOptions: {
-                browsers: [
-                    {
-                        name: BrowserName.firefox,
-                        minVersion: 96,
-                    },
-                ],
-                devices: [DeviceCategory.desktop],
-                operatingSystems: [OperatingSystemsName.windows],
-            },
-        },
-    },
-    launchContext: {
-        launcher: firefox,
-        launchOptions: await launchOptions({
-            headless: true,
-        }),
-    },
-    async requestHandler({ request, page, enqueueLinks, log, pushData }) {
-        if (page.url() === "https://pitergsm.ru/catalog/watch/") {
-            await enqueueLinks({
-                selector: 'a.prodcard__name'
-            });
-            return
-        }
 
+const parseWatchesPage = async (page: PlaywrightCrawlingContext["page"]) => {
+    await page.locator('[data-target="#tab-about"]').first().click()
+    await page.waitForSelector('.textoverflow__text')
+    const description = await page.locator('.textoverflow__text').innerHTML()
+
+    return {
+        category: 'watches',
+        description
+    }
+}
+
+
+const parsePhonesPage = async (page: PlaywrightCrawlingContext["page"]) => {
+    await page.locator('[data-target="#tab-specs"]').first().click()
+    await page.waitForSelector('#tab-specs')
+    const description = await page.locator('.textoverflow__text').first().innerHTML()
+
+    const cpu = await page
+        .locator('.pspecs__name', { hasText: /Процессор/ })
+        .first()
+        .locator('..')
+        .locator('.pspecs__val')
+        .first()
+        .textContent()
+
+    const screenSize = await page
+        .locator('.pspecs__name', { hasText: /Диагональ/ })
+        .first()
+        .locator('..')
+        .locator('.pspecs__val')
+        .first()
+        .textContent()
+
+    return {
+        category: 'headphones',
+        description,
+        screenSize,
+        cpu,
+        cpuCores: '',
+        mainCamera: '',
+        frontCamera: '',
+        batteryCapacity: '',
+        screenResolution: '',
+        screenRefreshRate: '',
+        pixelDensity: '',
+        screenType: '',
+        weight: ''
+    }
+}
+
+
+const parsers = {
+    watches: parseWatchesPage,
+    phones: parsePhonesPage
+}
+
+
+const crawler = new PlaywrightCrawler({
+    ...crawlerDefault,
+    async requestHandler({ request, page, enqueueLinks, log, pushData }) {
         const title = await page.locator('.section__title').first().innerText()
         const raw_price = await page.locator('.product__price').first().innerText()
-        const category = await page.locator('.breadcrumbs__item').nth(2).locator('a').first().innerText()
+        
         // const specsBrand = await page.locator('.specs__name', { hasText: "Бренд" })
+        
+        const additionalData = await parsers[request.label](page)
 
-        await page.locator('[data-target="#tab-about"]').first().click()
-        await page.waitForSelector('.textoverflow__text')
-        const description = await page.locator('.textoverflow__text').innerHTML()
-
-
-        await pushData({ title, raw_price, category, description });
+        await pushData({ title, raw_price, ...additionalData }, request.label);
 
         const image = await page.locator('.prodslider__pic-img').first().getAttribute('src')
 
-        try {
-            await downloadCrawler.addRequests([
-                'https://pitergsm.ru' + image
-            ])
-        } catch (error) {
-            console.error(error)
-        }
+        await downloadCrawler.addRequests([
+            'https://pitergsm.ru' + image
+        ])
+
+        await enqueueLinks({
+            selector: 'a.prodcard__name'
+        });
 
         // log.info(`Title of ${request.loadedUrl} is '${title}'`);
-    },
-    failedRequestHandler({ request, log }) {
-        log.error(`Request ${request.url} failed too many times.`);
-    },
-    maxRequestsPerCrawl: 10,
-    headless: true,
+    }
 });
 
-await crawler.run(['https://pitergsm.ru/catalog/watch/']);
+await crawler.run([
+    {
+        url: 'https://pitergsm.ru/catalog/watch/',
+        crawlDepth: 1,
+        label: "watches"
+    },
+    {
+        url: 'https://pitergsm.ru/catalog/phones/',
+        crawlDepth: 1,
+        label: "phones"
+    }
+]);
+
 await downloadCrawler.run()
