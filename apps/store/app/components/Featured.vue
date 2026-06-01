00f9c1fca231eea10d54e8416bc60a1041fb8a08
Egor <Egor@example.com>
2026-06-01 13:54:00 +0300

    featured

--- apps/store/app/components/Featured.vue
+++ apps/store/app/components/Featured.vue
@@ -1,44 +1,68 @@
 <script setup lang="ts">
-import { Button } from "@repo/ui";
 import airPodsImage from "~/assets/images/products/airpods_max.png";
+import appleVisionProImage from "~/assets/images/products/apple_vision_pro.png"
+import macbookAirImage from "~/assets/images/products/macbook_air.png"
+import playstation5Image from "~/assets/images/products/playstation_5.png"
 
 const products = [
   {
     title: "Apple AirPods Max",
     description: "Computational audio. Listen, it's powerful",
     image: airPodsImage,
+    background: '#EDEDED',
+    color: 'black',
+    descriptionColor: '#909090'
   },
   {
     title: "Apple Vision Pro",
     description: "An immersive way to experience entertainment",
-    image: airPodsImage,
+    image: appleVisionProImage,
+    background: '#353535',
+    color: 'white',
+    descriptionColor: '#909090'
   },
   {
     title: "Playstation 5",
     description:
       "Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.",
-    image: airPodsImage,
+    image: playstation5Image,
+    background: '#FFFFFF',
+    color: 'black',
+    descriptionColor: '#909090'
   },
   {
     title: "Macbook Air",
     description:
       "The new 15‑inch MacBook Air makes room for more of what you love with a spacious Liquid Retina display.",
-    image: airPodsImage,
+    image: macbookAirImage,
+    background: '#EDEDED',
+    color: 'black',
+    descriptionColor: '#909090'
   },
 ];
 </script>
 
 <template>
   <article class="featured">
-    <section class="featured-product" v-for="product in products">
-      <img class="product-image" :src="product.image" :alt="product.title" />
-      <h3 class="product-title">{{ product.title }}</h3>
-      <p class="product-description">{{ product.description }}</p>
+    <section :style="{ background: product.background }" class="featured-product" v-for="product in products">
+      <div class="image-wrapper">
+        <img class="product-image" :src="product.image" :alt="product.title" />
+      </div>
+      <div class="featured-product-text">
+      <h3 :style="{ color: product.color }" class="product-title">{{ product.title }}</h3>
+      <p :style="{ color: product.descriptionColor }" class="product-description">{{ product.description }}</p>
+      </div>
     </section>
   </article>
 </template>
 
 <style scoped>
+/* @import url('https://fonts.googleapis.com/css2?family=Abel&display=swap');
+
+* {
+  font-family: abel, sans-serif;
+} */
+
 .featured {
   display: flex;
   flex-direction: column;
@@ -48,9 +72,111 @@ const products = [
   flex-direction: column;
   align-items: center;
   text-align: center;
+  padding: 16px 40px;
+  gap: 8px;
 }
+
+.product-title {
+  font-size: 34px;
+  line-height: 40px;  
+}
+
+.product-description {
+  color: #909090;
+  font-size: 16px;
+}
+
+
 .product-image {
-  width: 80%;
+  max-height: 200px;
+  max-width: 80%;
   height: auto;
+}
+
+@media screen and (min-width: 1200px) {
+  .featured {
+    display: grid;
+    grid-template-columns: 1fr 1fr 2fr;
+    grid-template-rows: 328px 328px;
+  }
+  .product-image {
+    max-height: 272px;
+    max-width: 100%;
+    height: auto;
+  }
+  .featured-product {
+    display: flex;
+    flex-direction: row;
+    padding-left: 0;
+  }
+  .featured-product-text {
+    max-width: 339px;
+    text-align: left;
+  }
+
+  .featured-product:nth-child(1) {
+    grid-row-start: 2;
+  }
+  .featured-product:nth-child(1) .image-wrapper {
+    height: 100%;
+    width: 104px;
+  }
+  .featured-product:nth-child(1) .featured-product-text {
+    max-width: 160px;
+    padding: 0 20px;
+  }
+  .featured-product:nth-child(1) .product-image {
+    height: 100%;
+    object-fit: cover;
+    object-position: right;
+  }
+
+  .featured-product:nth-child(2) .image-wrapper {
+    height: 100%;
+    width: 300px;
+  }
+  .featured-product:nth-child(2) .product-image {
+    height: 100%;
+    object-fit: cover;
+    object-position: right;
+  }
+  
+  .featured-product:nth-child(3) {
+    grid-row-start: 1;
+    grid-column-start: 1;
+    grid-column-end: 3;
+  }
+  .featured-product:nth-child(3) .image-wrapper {
+    height: 343px;
+    width: 360px;
+  }
+  .featured-product:nth-child(3) .product-image {
+    height: 100%;
+    width: 100%;
+  }
+  .featured-product:nth-child(3) .featured-product-text {
+    max-width: 338px;
+    padding: 0 20px;
+  }
+
+  .featured-product:nth-child(4) {
+    grid-row-start: 1;
+    grid-row-end: 3;
+    align-items: center;
+    padding-left: 56px;
+    padding-right: 0;
+  }
+  .featured-product:nth-child(4) .image-wrapper {
+    height: 502px;
+    width: 300px;
+    order: 2;
+    display: flex;
+    align-items: center;
+  }
+  .featured-product:nth-child(4) .product-image {
+    height: 100%;
+    object-fit: cover;
+    object-position: left;
+  }
 }
 </style>
